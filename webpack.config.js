6eafd709d95d3374c3e4259d889ba67ac69548ce
Nadhir <nadhirxz@gmail.com>
2021-09-19 03:57:45 +0100

    Optimizations for output files

--- webpack.config.js
+++ webpack.config.js
@@ -15,17 +15,21 @@ const optimization = {
 	minimize: true,
 	minimizer: [
 		new TerserPlugin({
+			parallel: true,
 			extractComments: false,
+			terserOptions: {
+				output: { comments: false }
+			}
 		}),
 	],
 }
 
 const production = process.env.NODE_ENV == 'production';
-const compile = `'use strict'; const bytenode = require('bytenode'); const fs = require('fs'); const v8 = require('v8'); const path = require('path'); v8.setFlagsFromString('--no-lazy'); if (fs.existsSync(path.join(__dirname, './electron.js'))) { bytenode.compileFile(path.join(__dirname, './electron.js'), path.join(__dirname, './electron.jsc'));${production && ` fs.unlinkSync(path.join(__dirname, './electron.js'));`} }`
 
 module.exports = [
 	{
 		mode: production ? 'production' : 'development',
+		...(!production && { devtool: 'source-map' }),
 		entry: './src/electron.ts',
 		target: 'electron-main',
 		resolve: {
@@ -48,12 +52,12 @@ module.exports = [
 		plugins: [
 			new WebpackShellPluginNext({
 				onBuildEnd: {
-					scripts: ['IF not exist build mkdir build', ...production ? [`echo ${compile} process.exit(); > build/compile.js`, `electron build/compile.js && del build\\compile.js`, `echo require('bytenode'); require('./electron.jsc'); > build/main.js`] : [`echo ${compile} require('./electron.jsc'); > build/main.js`]],
+					scripts: ['IF not exist build mkdir build', ...production ? [`echo 'use strict'; const bytenode = require('bytenode'); const fs = require('fs'); const v8 = require('v8'); const path = require('path'); v8.setFlagsFromString('--no-lazy'); if (fs.existsSync(path.join(__dirname, './electron.js'))) { bytenode.compileFile(path.join(__dirname, './electron.js'), path.join(__dirname, './electron.jsc')); fs.unlinkSync(path.join(__dirname, './electron.js')); } process.exit(); > build/compile.js`, `electron build/compile.js && del build\\compile.js`, `echo require('bytenode'); require('./electron.jsc'); > build/main.js`] : [`echo require('./electron.js'); > build/main.js`]],
 					blocking: true,
 					parallel: false
 				}
 			}),
-			new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ['**/*', '!electron.js'] }),
+			new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ['**/*', '!electron.js', ...(!production && ['!electron.js.map'])] }),
 		],
 		optimization
 	},
