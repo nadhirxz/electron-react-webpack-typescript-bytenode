3cea3e7ea37e44823b766b1978b86ecb9dca46ac
Nadhir <nadhirxz@gmail.com>
2021-09-19 03:38:22 +0100

    Optimizations for building the app

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,31 @@
 require('dotenv').config();
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WebpackShellPluginNext = require('webpack-shell-plugin-next');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
 	template: __dirname + '/src/index.html',
 	filename: 'index.html',
 	inject: 'body'
 });
-const CreateFileWebpack = require('create-file-webpack');
+
+const optimization = {
+	minimize: true,
+	minimizer: [
+		new TerserPlugin({
+			extractComments: false,
+		}),
+	],
+}
+
+const production = process.env.NODE_ENV == 'production';
+const compile = `'use strict'; const bytenode = require('bytenode'); const fs = require('fs'); const v8 = require('v8'); const path = require('path'); v8.setFlagsFromString('--no-lazy'); if (fs.existsSync(path.join(__dirname, './electron.js'))) { bytenode.compileFile(path.join(__dirname, './electron.js'), path.join(__dirname, './electron.jsc'));${production && ` fs.unlinkSync(path.join(__dirname, './electron.js'));`} }`
 
 module.exports = [
 	{
-		mode: 'development',
+		mode: production ? 'production' : 'development',
 		entry: './src/electron.ts',
 		target: 'electron-main',
 		resolve: {
@@ -31,23 +46,27 @@ module.exports = [
 			filename: 'electron.js'
 		},
 		plugins: [
-			new CreateFileWebpack({
-				path: './build',
-				fileName: 'main.js',
-				content: `'use strict'; const bytenode = require('bytenode'); const fs = require('fs'); const v8 = require('v8'); const path = require('path'); v8.setFlagsFromString('--no-lazy'); if (fs.existsSync(path.join(__dirname, './electron.js'))) bytenode.compileFile(path.join(__dirname, './electron.js'), path.join(__dirname, './electron.jsc')); require('./electron.jsc');`
-			})
-		]
+			new WebpackShellPluginNext({
+				onBuildEnd: {
+					scripts: ['IF not exist build mkdir build', ...production ? [`echo ${compile} process.exit(); > build/compile.js`, `electron build/compile.js && del build\\compile.js`, `echo require('bytenode'); require('./electron.jsc'); > build/main.js`] : [`echo ${compile} require('./electron.jsc'); > build/main.js`]],
+					blocking: true,
+					parallel: false
+				}
+			}),
+			new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ['**/*', '!electron.js'] }),
+		],
+		optimization
 	},
 	{
-		mode: 'development',
+		mode: production ? 'production' : 'development',
+		...(!production && { devtool: 'source-map' }),
 		entry: './src/index.tsx',
 		target: 'electron-renderer',
 		output: {
-			path: __dirname + '/build/react',
+			path: path.join(__dirname, '/build/react'),
 			filename: 'bundle.js'
 		},
 		plugins: [HTMLWebpackPluginConfig],
-		devtool: 'source-map',
 		devServer: {
 			static: {
 				directory: path.join(__dirname, 'dist/react'),
@@ -69,6 +88,7 @@ module.exports = [
 					use: [{ loader: 'ts-loader' }]
 				}
 			]
-		}
+		},
+		optimization
 	}
 ];
